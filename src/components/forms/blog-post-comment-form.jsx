22759fbdb15910b0b87ef9c781e07665495fdb27
Simon G <142384587+itsmesimong@users.noreply.github.com>
2025-06-17 13:23:18 +0530

    Update blog-post-comment-form.jsx

--- src/components/forms/blog-post-comment-form.jsx
+++ src/components/forms/blog-post-comment-form.jsx
@@ -15,7 +15,7 @@ const BlogPostCommentForm = () => {
           </div>
           <div className="tp-postbox-details-input-box">
             <div className="tp-contact-input">
-              <input name="email" id="email" type="email" placeholder="sakthi@mail.com" />
+              <input name="email" id="email" type="email" placeholder="vishthavaa@mail.com" />
             </div>
             <div className="tp-postbox-details-input-title">
               <label htmlFor="email">Your Email</label>
