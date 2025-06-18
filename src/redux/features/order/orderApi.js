e49af4ee5a00c65ba79a649816f56f21df15c0b8
Simon G <142384587+itsmesimong@users.noreply.github.com>
2025-06-18 17:50:53 +0530

    Update orderApi.js

--- src/redux/features/order/orderApi.js
+++ src/redux/features/order/orderApi.js
@@ -7,7 +7,7 @@ export const authApi = apiSlice.injectEndpoints({
     // createPaymentIntent
     createPaymentIntent: builder.mutation({
       query: (data) => ({
-        url: "https://aihost.in/api/order/create-payment-intent",
+        url: "https://api.vishthavaa.com/api/order/create-payment-intent",
         method: "POST",
         body: data,
       }),
@@ -25,7 +25,7 @@ export const authApi = apiSlice.injectEndpoints({
     // saveOrder
     saveOrder: builder.mutation({
       query: (data) => ({
-        url: "https://aihost.in/api/order/saveOrder",
+        url: "https://api.vishthavaa.com/api/order/saveOrder",
         method: "POST",
         body: data,
       }),
@@ -46,13 +46,13 @@ export const authApi = apiSlice.injectEndpoints({
     }),
     // getUserOrders
     getUserOrders: builder.query({
-      query: () => `https://aihost.in/api/user-order`,
+      query: () => `https://api.vishthavaa.com/api/user-order`,
       providesTags:["UserOrders"],
       keepUnusedDataFor: 600,
     }),
     // getUserOrders
     getUserOrderById: builder.query({
-      query: (id) => `https://aihost.in/api/user-order/${id}`,
+      query: (id) => `https://api.vishthavaa.com/api/user-order/${id}`,
       providesTags: (result, error, arg) => [{ type: "UserOrder", id: arg }],
       keepUnusedDataFor: 600,
     }),
