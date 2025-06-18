971934e23205a09ac8688fde7b4958197261569b
Simon G <142384587+itsmesimong@users.noreply.github.com>
2025-06-18 17:48:49 +0530

    Update reviewApi.js

--- src/redux/features/reviewApi.js
+++ src/redux/features/reviewApi.js
@@ -5,7 +5,7 @@ export const reviewApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     addReview: builder.mutation({
       query: (data) => ({
-        url: "https://aihost.in/api/review/add",
+        url: "https://api.vishthavaa.com/api/review/add",
         method: "POST",
         body: data,
       }),
